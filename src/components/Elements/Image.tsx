a1c9a90cbd29626ab2e43e4b70b56d07b559e88f
Naing Linn Khant <nainglinnkhnt@gmail.com>
2023-01-26 09:59:59 +0630

    Add fallback image in Image component

--- src/components/Elements/Image.tsx
+++ src/components/Elements/Image.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import fallback from '../../assets/poster-fallback.jpg'
 import styles from './Image.module.css'
 
 interface ImageProps extends React.HTMLProps<HTMLImageElement> {
@@ -10,18 +11,33 @@ interface ImageProps extends React.HTMLProps<HTMLImageElement> {
 
 const Image = (props: ImageProps) => {
     const [imageLoaded, setImageLoaded] = useState(false)
+    const [imageSrc, setImageSrc] = useState(props.src)
+
+    const imageRef = useRef<HTMLImageElement>(null)
     
     const { skeleton = true, skeletonClassName, width, height, ...imageProps } = props
 
     const imageClass = imageLoaded ? 'd-block' : 'd-none'
 
+    useEffect(() => {
+        const handleImageError = () => setImageSrc(fallback)
+
+        const imageElement = imageRef.current
+        
+        imageElement?.addEventListener('error', handleImageError)
+
+        return () => imageElement?.removeEventListener('error', handleImageError)
+    }, [])
+
     return (
         <>
             <img
+                ref={imageRef}
                 {...imageProps}
+                src={imageSrc}
                 alt={imageProps.alt}
                 onLoad={() => setImageLoaded(true)}
-                className={`${styles.image} ${skeleton ? imageClass : ''}`}
+                className={`${styles.image} ${skeleton ? imageClass : ''} ${imageProps.className}`}
                 style={{ width, height }}
             />
 
